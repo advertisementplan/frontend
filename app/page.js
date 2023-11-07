194550e3fe46adf681097069156868e2b8831b0f
Gaurav <gaurav1223344@gmail.com>
2023-11-07 20:16:43 +0530

    NewGoodHabit completed

--- app/page.js
+++ app/page.js
@@ -3,15 +3,19 @@ import { ProductSlider } from './src/components/group1/ProductSlider'
 import { Discount } from './src/components/group1/Discount'
 import {NextUIProvider} from "@nextui-org/react";
 import { Group2 } from './src/components/Group2';
+import { NewGoodHabit } from './src/components/NewGoodHabit';
+import { ProceedButton } from './src/components/ProceedButton';
 
 export default function Home() {
   return (
     <NextUIProvider>
       <main className="flex min-h-screen flex-col items-center justify-between p-[25px]" >
         <div className='container flex flex-col items-center justify-center'>
-            <ProductSlider/>
+            {/* <ProductSlider/>
             <Discount/>
-            <Group2/>
+            <Group2/> */}
+            <ProceedButton/>
+            {/* <NewGoodHabit/> */}
         </div>
     </main>
     </NextUIProvider>
---
+++ app/src/components/ProceedButton.js
@@ -0,0 +1,11 @@
+// import React from "react";
+
+
+export const ProceedButton = () => {
+    return (
+        <div></div>
+    );
+  };
+  
+ 
+  
